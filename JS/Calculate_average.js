12258213f4785272428a24d88c8dc13111595778
AkiMira <akimiratest@gmail.com>
2023-07-31 16:19:51 +0300

    31/07/16:19

---
+++ JS/Calculate_average.js
@@ -0,0 +1,13 @@
+// DECRIPTION:
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+// SOLUTION:
+function findAverage(array) {
+    if (array.length == 0) {
+      return 0;
+      } else {
+      return array.reduce((a, b) => (a + b)) / array.length;
+  }
+    }
