772e3006d3ff0d0fd2e0fbd5bc9853b413c97cd3
AkiMira <akimiratest@gmail.com>
2023-08-07 19:12:18 +0300

    07/08/19:12

---
+++ JS/Remove_exclamation_marks.js
@@ -0,0 +1,7 @@
+// DECRIPTION:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+// SOLUTION:
+function removeExclamationMarks(s) {
+    let newStr = s.replace(/!/g, "");
+    return newStr;
+  }
