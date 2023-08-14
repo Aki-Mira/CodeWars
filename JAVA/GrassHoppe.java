c1fba1db9cbf0c44e794c34d6a3e0bef989d8ed3
AkiMira <akimiratest@gmail.com>
2023-08-14 18:17:14 +0300

    14/08

---
+++ JAVA/GrassHoppe.java
@@ -0,0 +1,9 @@
+public class GrassHopper {
+  public static void main(String[] args){
+        
+    }
+    
+   public static String sayHello(String name) {
+        return "Hello, " + name;
+    }
+}
